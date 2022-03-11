960231f9aeb226b76a74e1fa9c96bfd65d788215
JMT17300 <95630637+JMT17300@users.noreply.github.com>
2022-03-11 10:33:55 +0100

    vendredi 10h30

--- greenGarden3/js/script.js
+++ greenGarden3/js/script.js
@@ -1,23 +1,22 @@
-//const regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
+
 const btn = document.getElementById('boton');
 
-
 btn.addEventListener('click', function () {
-    let lastName = document.getElementById('lastName'); //nom
-    let span1 = document.getElementById('sp1'); // Span nom
+    let lastName = document.getElementById('lastName'); //prenom
+    let span1 = document.getElementById('sp1'); // Span prenom
 
     ///////////////////////////////////////////////////////////////
 
-    let newName = document.getElementById('name'); //prenom
-    let span2 = document.getElementById('sp2'); // Span prenom
+    let newName = document.getElementById('name'); //nom
+    let span2 = document.getElementById('sp2'); // Span nom
 
     //////////////////////////////////////////////////////////////
     let date = document.getElementById('date'); //date
     let span4 = document.getElementById('sp4'); // Span date
 
-//////////////////////////////////////////////////////////////
-let mail = document.getElementById('mail'); //mail
-let span5 = document.getElementById('sp5'); // Span mail
+    //////////////////////////////////////////////////////////////
+    let mail = document.getElementById('mail'); //mail
+    let span5 = document.getElementById('sp5'); // Span mail
 
 
     let pw = document.getElementById('pw'); //password
@@ -25,7 +24,7 @@ let span5 = document.getElementById('sp5'); // Span mail
     let span3 = document.getElementById('sp3'); // span confirm
 
 
-    //confirmer prenom
+    //confirmer longeur prenom
     if (lastName.value.length <= 2) {
         span1.classList.add('error');
         lastName.value = '';
@@ -35,7 +34,7 @@ let span5 = document.getElementById('sp5'); // Span mail
     }
 
 
-    //confirmer nom
+    //confirmer longeur nom
     if (newName.value.length <= 2) {
         span2.classList.add('error');
         newName.value = '';
@@ -45,33 +44,34 @@ let span5 = document.getElementById('sp5'); // Span mail
     }
 
     //confirmer date
-    if (date.value.length <= 2) {
+    if (date.value.length <= 7) {
         span4.classList.add('error');
         date.value = '';
     } else {
         span4.classList.remove('error');
-        console.log("ok nom");
+        console.log("ok date");
     }
 
- //confirmer mail
- if (mail.value.length <= 2) {
-    span5.classList.add('error');
-    mail.value = '';
-} else {
-    span5.classList.remove('error');
-    console.log("ok nom");
-}
+    //confirmer mail
+    if (mail.value.length <= 2) {
+        span5.classList.add('error');
+        mail.value = '';
+    } else {
+        span5.classList.remove('error');
+        console.log("ok mail");
+    }
 
     //confirm Password
 
     if (pw.value === cPw.value) {
         span3.classList.remove('error');
-
+        console.log("ok mdp");
     } else {
         span3.classList.add('error'); //option 1
         span3.innerHTML = 'password incorrect'; // option 2
         pw.value = '';
         cPw.value = '';
+
     }
 })
 
